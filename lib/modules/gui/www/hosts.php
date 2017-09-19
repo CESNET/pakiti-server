464f1d1cba596beee5cf08ab4fe3d740c3b7c3b2
mlcit <jakub.mlcak@seznam.cz>
2017-09-19 05:27:19 +0200

    show list of tagged CVEs only with tag in search form - submit search form after change select

--- lib/modules/gui/www/hosts.php
+++ lib/modules/gui/www/hosts.php
@@ -110,7 +110,7 @@ $activity = array("Last 24 hours" => "24h", "Last 2 days" => "2d", "Last week" =
                 <div class="col-sm-3">
                     <div class="form-group">
                         <label for="cveName">CVE</label>
-                        <select class="form-control" name="cveName" id="cveName">
+                        <select class="form-control" name="cveName" id="cveName" onchange="submit();">
                             <option value="">All</option>
                             <?php foreach ($cveNames as $cveName) { ?>
                                 <option value="<?php echo $cveName; ?>"<?php if ($_cveName === $cveName) echo ' selected'; ?>><?php echo $cveName; ?></option>
@@ -121,7 +121,7 @@ $activity = array("Last 24 hours" => "24h", "Last 2 days" => "2d", "Last week" =
                 <div class="col-sm-3">
                     <div class="form-group">
                         <label for="tag">CVE Tag</label>
-                        <select class="form-control" name="tag" id="tag">
+                        <select class="form-control" name="tag" id="tag" onchange="submit();">
                             <option value="">All</option>
                             <option value="true"<?php if ($_tag === true) echo ' selected'; ?>>Any tag</option>
                             <?php foreach ($tagNames as $tagName) { ?>
@@ -133,7 +133,7 @@ $activity = array("Last 24 hours" => "24h", "Last 2 days" => "2d", "Last week" =
                 <div class="col-sm-3">
                     <div class="form-group">
                         <label for="activity">Activity</label>
-                        <select class="form-control" name="activity" id="activity">
+                        <select class="form-control" name="activity" id="activity" onchange="submit();">
                             <option value="">All</option>
                             <?php foreach ($activity as $key => $value) { ?>
                                 <option value="<?php echo $value; ?>"<?php if ($value == $_activity) echo ' selected'; ?>><?php echo $key; ?></option>
@@ -147,7 +147,7 @@ $activity = array("Last 24 hours" => "24h", "Last 2 days" => "2d", "Last week" =
                 <div class="col-sm-3">
                     <div class="form-group">
                         <label for="hostGroupId">Host group</label>
-                        <select class="form-control" name="hostGroupId" id="hostGroupId">
+                        <select class="form-control" name="hostGroupId" id="hostGroupId" onchange="submit();">
                             <option value="">All</option>
                             <?php foreach ($hostGroups as $hostGroup) { ?>
                                 <option value="<?php echo $hostGroup->getId(); ?>"<?php if ($hostGroup->getId() == $_hostGroupId) echo ' selected'; ?>><?php echo $hostGroup->getName(); ?></option>
@@ -217,6 +217,9 @@ $activity = array("Last 24 hours" => "24h", "Last 2 days" => "2d", "Last week" =
                     <?php } ?>
                 <?php } ?>
                 <input name="listTaggedCves" type="checkbox" title="show list of CVEs" onchange="location.href='<?php echo $html->getQueryString(array("listTaggedCves" => ($_listTaggedCves ? "" : "true"))); ?>';" <?php if($_listTaggedCves) echo ' checked'; ?>>
+                <?php if($_listTaggedCves && $_tag !== null && $_tag !== true) { ?>
+                    <br><small class="text-normal">(only <?php echo $_tag; ?>)</small>
+                <?php } ?>
             </th>
             <th>#Reports</th>
             <th>
@@ -256,7 +259,7 @@ $activity = array("Last 24 hours" => "24h", "Last 2 days" => "2d", "Last week" =
                 </td>
                 <td>
                     <?php if ($_listTaggedCves) { ?>
-                        <?php foreach ($html->getPakiti()->getManager("CvesManager")->getCvesNamesForHost($host->getId(), true) as $cveName) { ?>
+                        <?php foreach ($html->getPakiti()->getManager("CvesManager")->getCvesNamesForHost($host->getId(), ($_tag == null ? true : $_tag)) as $cveName) { ?>
                             <a href="cve.php?cveName=<?php echo $cveName; ?>" class="text-danger"><?php echo $cveName; ?></a><br>
                         <?php } ?>
                     <?php } else { ?>
