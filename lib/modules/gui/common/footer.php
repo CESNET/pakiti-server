6ddd444ec94ccc99d8cd02bc73f36881c86a099b
Daniel Kouril <kouril@ics.muni.cz>
2018-01-15 14:48:53 +0100

    Bumped year

--- lib/modules/gui/common/footer.php
+++ lib/modules/gui/common/footer.php
@@ -29,7 +29,7 @@
 ?>
 
         <div class="text-center"><?php echo Config::$GUI_FOOTER; ?></div>
-        <div class="text-center">Copyright &copy; 2017, CESNET, <a href="<?php echo Constants::$PAKITI_GITHUB; ?>" target="_blank">GitHub</a></div>
+        <div class="text-center">Copyright &copy; 2018, CESNET, <a href="<?php echo Constants::$PAKITI_GITHUB; ?>" target="_blank">GitHub</a></div>
     </div>
 </body>
 
