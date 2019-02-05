5a989803ca040a89dc035d0321d9a4b7db2d3d14
Daniel Kouril <kouril@ics.muni.cz>
2019-02-05 07:27:11 +0100

    Remove year from the footers

--- src/modules/gui/www/public/index.php
+++ src/modules/gui/www/public/index.php
@@ -125,7 +125,7 @@ Pakiti has a web based GUI which provides a list of registered systems. The coll
             </div>
         </div>
         <div class="text-center"><?php echo Config::$GUI_FOOTER; ?></div>
-        <div class="text-center">Copyright &copy; 2018, CESNET, <a href="<?php echo Constants::$PAKITI_GITHUB; ?>" target="_blank">GitHub</a></div>
+        <div class="text-center">Copyright &copy; CESNET, <a href="<?php echo Constants::$PAKITI_GITHUB; ?>" target="_blank">GitHub</a></div>
     </div>
 </body>
 
